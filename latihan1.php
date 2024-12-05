cd7eb6d65fcd9208fe3a9276072a6fa1c39d1173
ronandasaputra <ronandasaputra@gmail.com>
2024-12-05 16:23:00 +0700

    first commit

--- latihan1.php
+++ latihan1.php
@@ -1,12 +1,8 @@
 <?php
     // Soal 1
-    // Variabel
     $nama = "RuangKoding";
     $umur = 25;
-
-    // Output
     echo "Halo, nama saya $nama dan umur saya $umur tahun.";
-
 
     // Soal 2
     $hobi = ["Coding", "Membaca", "Bermain musik"];
